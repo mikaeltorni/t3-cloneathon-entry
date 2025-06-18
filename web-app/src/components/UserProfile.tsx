e0da49b723d156aca373d0edda1d878830f82b8d
Mikael Törni <mikaeltorni25@gmail.com>
2025-06-18 17:50:57 +0300

    Made in one week

--- web-app/src/components/UserProfile.tsx
+++ web-app/src/components/UserProfile.tsx
@@ -127,7 +127,7 @@ export const UserProfile: React.FC = () => {
       {/* Project Information */}
       <div className="mt-2 text-xs text-gray-400 dark:text-slate-500 space-y-1">
         <div>v18062025</div>
-        <div>Made by Mikael Törni</div>
+        <div>Made in one week by Mikael Törni</div>
         <a 
           href="https://github.com/mikaeltorni/t3-cloneathon-entry" 
           target="_blank" 
